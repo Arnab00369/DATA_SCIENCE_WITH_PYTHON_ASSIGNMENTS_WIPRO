450a8eeac026203504e7617540b09e1c6d7ba63f
Arnab Jana <arnab003jn.369@gmail.com>
2025-07-18 22:07:22 +0530

    1st change on python folder

---
+++ Python_Fundamentals/Flow_Control_Statements/1_Number_Check.py
@@ -0,0 +1,8 @@
+number = float(input("Enter a number: "))
+print("\nOutput is:")
+if(number>0):
+   print(number," is a positive number")
+elif(number<0):
+   print(number," is a negative number")
+else:
+   print(number," is zero")
--- Python_Fundamentals/Flow_Control_Statements/Number_Check.py
+++
@@ -1,8 +0,0 @@
-a = float(input("Enter a number: "))
-print("\nOutput is:")
-if(a>0):
-   print(a," is a positive number")
-elif(a<0):
-   print(a," is a negative number")
-else:
-   print(a," is zero")
