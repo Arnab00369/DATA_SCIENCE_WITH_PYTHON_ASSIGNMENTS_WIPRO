963fecb3d5f905d91febbfdad8c90e58d2ed010b
Arnab Jana <arnab003jn.369@gmail.com>
2025-07-22 22:15:49 +0530

    3rd file done

--- Python_Fundamentals/Flow_Control_Statements/3_Last_Digit_Same.py
+++ Python_Fundamentals/Flow_Control_Statements/3_Last_Digit_Same.py
@@ -6,12 +6,12 @@
 number_1 = int(input("Enter a non-negative number: "))
 number_2 = int(input("Enter another non-negative number: "))
 print("\nOutput:")
-if(number_1 > 0 & number_2 > 0):
-   if(number_1 % 10 == number_2 % 10):
+if((number_1 >= 0) & (number_2 >= 0)):
+   if((number_1 % 10) == (number_2 % 10)):
       print(number_1," and ",number_2," have Same Digits")
    else:
       print(number_1," and ",number_2," have Different Digits")
-elif(number_1 > 0 & number_2 < 0):
+elif((number_1 > 0) & (number_2 < 0)):
    print(number_2," is a negative number so cannot check any further. Please check if both the numbers are non-negative")
 elif((number_2 > 0) & (number_1 < 0)):
    print(number_1," is a negative number so cannot check any further. Please check if both the numbers are non-negative")
