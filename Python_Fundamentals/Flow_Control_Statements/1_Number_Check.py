c9c3464bed0f890fe172c0accba00371a22b8542
Arnab Jana <arnab003jn.369@gmail.com>
2025-07-22 22:03:23 +0530

    Done

--- Python_Fundamentals/Flow_Control_Statements/1_Number_Check.py
+++ Python_Fundamentals/Flow_Control_Statements/1_Number_Check.py
@@ -1,6 +1,6 @@
 # Question 1: Write a program to check if a given number is Positive, Negative, or Zero.
 
-number = float(input("Enter a number: "))
+number = int(input("Enter a number: "))
 print("\nOutput is:")
 if(number>0):
    print(number," is a positive number")
