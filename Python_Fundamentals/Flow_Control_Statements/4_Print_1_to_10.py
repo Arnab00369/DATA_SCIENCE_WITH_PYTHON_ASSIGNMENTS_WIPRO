7528597001cf56e465f4e52047c112147fc4ae89
Arnab Jana <arnab003jn.369@gmail.com>
2025-07-22 21:50:09 +0530

    4th and 5th file

--- Python_Fundamentals/Flow_Control_Statements/4_Print_1_to_10.py
+++ Python_Fundamentals/Flow_Control_Statements/4_Print_1_to_10.py
@@ -1,3 +1,5 @@
+# Question: Write a program to print numbers from 1 to 10 in a single row with one tab space.
+
 print("Number from 1 to 10 with one tab space are as follows:")
 for number in range(1, 11):
       print(number, end="  ")
--- Python_Fundamentals/Flow_Control_Statements/5_Even_from_23_and_57.py
+++ Python_Fundamentals/Flow_Control_Statements/5_Even_from_23_and_57.py
@@ -1,3 +1,5 @@
+# Question: Write a program to print even numbers between 23 and 57. Each number should be printed in a separate row.
+
 print("Even Numbers in between 23 and 57 in seperate rows are as follows:")
 for number in range(23, 58):
    if number % 2 == 0:
