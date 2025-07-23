34313fe21a94c7465dc07d8e2d60cb1456b502e3
Arnab Jana <arnab003jn.369@gmail.com>
2025-07-23 12:31:14 +0530

    Project_2

---
+++ Python_Fundamentals/TM03_Project2/Project2_PBLApp.py
@@ -0,0 +1,8 @@
+# Project 2: Let's assume you are planning to use your Python skills to build a PBLApp for Mobile. You decide to host your application on servers running in the cloud. You pick a hosting provider that charges $0.51 per hour. You will launch your service using one server and want to know how much it will cost to operate per day,per week,per month. 
+
+# Write a Python program that displays the answers to the following questions:
+
+# How much does it cost to operate one server per day?
+# How much does it cost to operate one server per week?
+# How much does it cost to operate one server per month?
+# How many days can I operate one server with $918?
