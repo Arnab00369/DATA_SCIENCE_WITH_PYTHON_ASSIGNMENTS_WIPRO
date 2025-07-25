95ac73906bece36f1bc937e3d58923c24052e577
Arnab Jana <arnab003jn.369@gmail.com>
2025-07-25 22:42:34 +0530

    4th file

---
+++ 02_Data_Structures/Set/04_Maximum_and_Minimum_Elements.py
@@ -0,0 +1,11 @@
+set_data = {60, 20, 40, 50.9, 90, 100.6}
+
+print('The elements of the set are:')
+for item in set_data:
+   print(item)
+
+max_element = max(set_data)
+min_element = min(set_data)
+
+print("\nThe maximum element in the set is:", max_element)
+print("The minimum element in the set is:", min_element)
