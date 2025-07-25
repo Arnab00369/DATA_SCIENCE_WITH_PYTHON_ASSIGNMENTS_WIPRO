59cf6d022d3a568263e917db80576c5a182b94cb
Arnab Jana <arnab003jn.369@gmail.com>
2025-07-25 22:27:45 +0530

    2nd done

--- 02_Data_Structures/Set/02_Set_Intersection.py
+++ 02_Data_Structures/Set/02_Set_Intersection.py
@@ -3,7 +3,12 @@ print('The elements of the Set 1 are:')
 for item in set_1:
    print(item)
 
-set_2 = {32, 'Yes', 'Data', 50, 'Intersection'}
+set_2 = {60, 'Yes', 'Arnab', 50, 16, 'Intersection'}
 print('\nThe elements of the Set 2 are:')
 for item in set_2:
+   print(item)
+
+set_3_intersect = set_1.intersection(set_2)
+print('\nThe intersection of Set 1 and Set 2 (common elements between both sets) is(are):')
+for item in set_3_intersect:
    print(item)
