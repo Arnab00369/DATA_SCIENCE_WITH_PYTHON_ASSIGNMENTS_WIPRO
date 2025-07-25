085ef9530e9a0104530333b42fd80909f69e57c4
Arnab Jana <arnab003jn.369@gmail.com>
2025-07-25 22:17:09 +0530

    2nd file

---
+++ 02_Data_Structures/Set/02_Set_Intersection.py
@@ -0,0 +1,9 @@
+set_1 = {60, 'Practice', 'Arnab', 16, 'Set'}
+print('The elements of the Set 1 are:')
+for item in set_1:
+   print(item)
+
+set_2 = {32, 'Yes', 'Data', 50, 'Intersection'}
+print('\nThe elements of the Set 2 are:')
+for item in set_2:
+   print(item)
