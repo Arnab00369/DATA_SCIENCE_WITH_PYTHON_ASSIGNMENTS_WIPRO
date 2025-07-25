45ca990195c6e38e3b69b6aa06911fce562cd57d
Arnab Jana <arnab003jn.369@gmail.com>
2025-07-25 22:30:10 +0530

    3rd file done

---
+++ 02_Data_Structures/Set/03_Set_Union.py
@@ -0,0 +1,14 @@
+set_1 = {60, 'Practice', 'Arnab', 16, 'Set'}
+print('The elements of the Set 1 are:')
+for item in set_1:
+   print(item)
+
+set_2 = {10, 'Yes', 'Data', 50, 'Intersection'}
+print('\nThe elements of the Set 2 are:')
+for item in set_2:
+   print(item)
+
+set_3_union = set_1.union(set_2)
+print('\nThe union of Set 1 and Set 2 is(are):')
+for item in set_3_union:
+   print(item)
