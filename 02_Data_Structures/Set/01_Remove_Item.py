5d5d800c99d41ca67217863d9c6824c86a1b4e72
Arnab Jana <arnab003jn.369@gmail.com>
2025-07-25 21:56:49 +0530

    changes

--- 02_Data_Structures/Set/01_Remove_Item.py
+++ 02_Data_Structures/Set/01_Remove_Item.py
@@ -1,10 +1,15 @@
 set_1 = {'Arnab', 36, 'Python', 'true', 10.6,  20}
+print('The elements of set already defined are: ')
+for item in set_1:
+   print(item)
 
-print('The set already created is: ',set_1)
 
-item_remove = input("Enter the item you want to remove from the set: ")
+item_remove = input("\nEnter the item you want to remove from the set: ")
 
 if item_remove in set_1:
    set_1.remove(item_remove)
    print("Item ",item_remove," has been removed from the set.") 
    print('The new set with removed item is: ',set_1)
+else:
+   print("Item ",item_remove," is not present in the set. No changes made.")
+   print('The set remains unchanged: ',set_1)
