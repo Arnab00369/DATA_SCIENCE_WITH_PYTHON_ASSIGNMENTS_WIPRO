1e69db74a0d13b223531298e17bf12917e38d8d7
Arnab Jana <arnab003jn.369@gmail.com>
2025-07-25 22:07:56 +0530

    Done finally

--- 02_Data_Structures/Set/01_Remove_Item.py
+++ 02_Data_Structures/Set/01_Remove_Item.py
@@ -1,4 +1,4 @@
-set_1 = {'Arnab', 36, 'Python', 'true', 10.6,  20}
+set_1 = {'Arnab', 36, 'Python', 'true', 10,  20}
 print('The elements of set already defined are: ')
 for item in set_1:
    print(item)
@@ -6,10 +6,17 @@ for item in set_1:
 
 item_remove = input("\nEnter the item you want to remove from the set: ")
 
+if item_remove.isdigit():
+   item_remove = int(item_remove)
+
 if item_remove in set_1:
-   set_1.remove(item_remove)
-   print("Item ",item_remove," has been removed from the set.") 
-   print('The new set with removed item is: ',set_1)
+      set_1.remove(item_remove)
+      print("Item ",item_remove," has been removed from the set.") 
+      print('The new set with removed item is: ')
+      for item in set_1:
+         print(item)
 else:
-   print("Item ",item_remove," is not present in the set. No changes made.")
-   print('The set remains unchanged: ',set_1)
+      print("Item ",item_remove," is not present in the set. No changes made.")
+      print('The set remains unchanged: ')
+      for item in set_1:
+         print(item)
