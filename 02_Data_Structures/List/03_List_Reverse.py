8aec2fd87e96b82d1b3eb368adca57e16d500b21
Arnab Jana <arnab003jn.369@gmail.com>
2025-07-26 21:41:26 +0530

    Done 3rd file

---
+++ 02_Data_Structures/List/03_List_Reverse.py
@@ -0,0 +1,10 @@
+list_1 = [20, 'Arnab', '@', 36.8, 'Data']
+
+print("The elements in the list in original order are as follows:")
+for item in list_1:
+   print(item)
+
+list_1.reverse()
+print("\nThe elements in the list in reverse order are as follows:")
+for item in list_1:
+   print(item)
