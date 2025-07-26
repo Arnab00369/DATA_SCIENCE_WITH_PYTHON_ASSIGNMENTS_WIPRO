29d7b0f57ad96ce0f35e6649d84cd11fc16865ac
Arnab Jana <arnab003jn.369@gmail.com>
2025-07-26 21:37:21 +0530

    2nd file chages made

--- 02_Data_Structures/List/02_Append_Item.py
+++ 02_Data_Structures/List/02_Append_Item.py
@@ -1,5 +1,10 @@
 list_new = [1, '2', 'Arnab', 40.6, 'true']
 
+print("The elements in the list before append are as follows:")
+for item in list_new:
+   print(item)
 list_new.append(6)
 
-print(list_new)
+print("The elements in the list after append are as follows:")
+for item in list_new:
+   print(item)
