b2e7828e7ee4eeb62a269d95ea9da9f326afc7ca
Arnab Jana <arnab003jn.369@gmail.com>
2025-07-26 22:04:03 +0530

    4th file done

--- 02_Data_Structures/List/04_Occurence_Item.py
+++ 02_Data_Structures/List/04_Occurence_Item.py
@@ -1,6 +1,9 @@
 list_input = input("Enter the elements of the list separated by commas: ")
 
-list_new = list.split(',')
+list_new = list_input.split(',')
 print("The elements in the list are as follows:")
 for item in list_new:
-   print(item)
+   print(item.strip())
+
+item_count = input("\nEnter the element in the list whose frequency you want to find: ")
+occurence = list_new.count()
