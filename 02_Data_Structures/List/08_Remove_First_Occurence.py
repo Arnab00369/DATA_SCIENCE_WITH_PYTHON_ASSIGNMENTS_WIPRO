7d466c178dcaed9cde4a9164e5e575a20e7238bb
Arnab Jana <arnab003jn.369@gmail.com>
2025-07-27 11:13:04 +0530

    8th file odne

--- 02_Data_Structures/List/08_Remove_First_Occurence.py
+++ 02_Data_Structures/List/08_Remove_First_Occurence.py
@@ -3,4 +3,15 @@ list_new = input("Enter the elements of the list separated by commas: ").split('
 print("The elements in the list are:")
 for items in list_new:
    print(items)
-   
+
+item_to_remove = input("\nEnter the element whose first occurrence is to be removed: ")
+
+if item_to_remove in list_new:
+   print("Removing the first occurrence of:", item_to_remove)
+   list_new.remove(item_to_remove)
+else:
+   print("Item not found in the list. Enter element which is present in the list.")
+
+print("\nThe elements in the list after removal of ",item_to_remove," are:")
+for items in list_new:
+   print(items)
