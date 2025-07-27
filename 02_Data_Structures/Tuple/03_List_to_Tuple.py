fcff2207199f4f0ce38097cdff9b5bb7818c23d5
Arnab Jana <arnab003jn.369@gmail.com>
2025-07-27 19:19:44 +0530

    3rd file done

--- 02_Data_Structures/Tuple/03_List_to_Tuple.py
+++ 02_Data_Structures/Tuple/03_List_to_Tuple.py
@@ -1,4 +1,15 @@
 list_1 = [1, 2, 30, 40, 500, 600, 7000, 8000, 900000]
 
+print("The elements of List are::\n", list_1, "\n")
+counter = 0
+for items in list_1:
+   counter += 1
+   print("Value ",counter," = ",items)
+
+# Coverting list to tuple
 tuple_1 = tuple(list_1)
-print(tuple_1)
+print("\nThe elements of Tuple after converting from list are::\n",tuple_1, "\n")
+counter_1 = 0
+for items in list_1:
+   counter_1 += 1
+   print("Value ",counter_1," = ",items)
