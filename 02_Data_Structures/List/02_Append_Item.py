6ebfa28528ff3dac9677caff0c12b8b21aa6d562
Arnab Jana <arnab003jn.369@gmail.com>
2025-07-27 22:27:16 +0530

    2nd file

--- 02_Data_Structures/List/02_Append_Item.py
+++ 02_Data_Structures/List/02_Append_Item.py
@@ -1,10 +1,16 @@
+# Question 2:
+# Write a program to append a new item to the end of the list.
+
+# Creating a list with mixed data types
 list_new = [1, '2', 'Arnab', 40.6, 'true']
 
+# Displaying the list items before appending
 print("The elements in the list before append are as follows:")
 for item in list_new:
    print(item)
 list_new.append(6)
 
+# Displaying the list items after appending
 print("\nThe elements in the list after append are as follows:")
 for item in list_new:
    print(item)
