aa2f48b24d4ceb3176b8cd6dab83e69186e12aa6
Arnab Jana <arnab003jn.369@gmail.com>
2025-07-27 19:13:53 +0530

    modiefied

--- 02_Data_Structures/Tuple/02_Tuple_Element_Existence.py
+++ 02_Data_Structures/Tuple/02_Tuple_Element_Existence.py
@@ -4,3 +4,10 @@ counter = 0
 for items in tuple_1:
    counter += 1
    print("Value ",counter," = ",items)
+
+item_find = input("\nEnter an element to find it exists in the tuple or not: ")
+
+if item_find in tuple_1:
+   print("Element", item_find, "exists in the tuple.")
+else:
+   print("Element", item_find, "does not exist in the tuple.")
