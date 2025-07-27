8537dddd842545afce206801fac17f3b60b1d837
Arnab Jana <arnab003jn.369@gmail.com>
2025-07-27 20:11:10 +0530

    2nd file done

--- 02_Data_Structures/String/02_String_Palindrome.py
+++ 02_Data_Structures/String/02_String_Palindrome.py
@@ -1,7 +1,8 @@
 String_1 = input("Enter a String to check if it is a Palindrome String or not: ")
-
+print("\nReverse of String: ", String_1[::-1])
+String_cpy = String_1
 String_1 = String_1.lower()  # Convert to lowercase for case-insensitive comparison
 if String_1 == String_1[::-1]:
-      print(String_1," is a Palindrome String")
+      print(String_cpy," is a Palindrome String")
 else:
-      print(String_1," is not a Palindrome String")
+      print(String_cpy," is not a Palindrome String")
