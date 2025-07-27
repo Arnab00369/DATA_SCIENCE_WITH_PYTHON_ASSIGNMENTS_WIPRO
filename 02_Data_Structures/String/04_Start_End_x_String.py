2cb01bd09fbfed680b8d4038a2892d67a23150c2
Arnab Jana <arnab003jn.369@gmail.com>
2025-07-27 20:19:33 +0530

    4th file ocmpleted

--- 02_Data_Structures/String/04_Start_End_x_String.py
+++ 02_Data_Structures/String/04_Start_End_x_String.py
@@ -4,4 +4,8 @@ if String_1.startswith('x'):
    String_1 = String_1[1:]
    if String_1.endswith('x'):
       String_1 = String_1[:-1]
-      print(String_1)
+      print("\nOUTPUT::\nResulting String: ",String_1)
+   else:
+      print("\nString should start and end with 'x' to give an output.")
+else:
+      print("\nString should start and end with 'x' to give an output.")
