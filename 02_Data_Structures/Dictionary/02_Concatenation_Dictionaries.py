b22a630a71e0421271bf04fe3fc8d324c6a468a9
Arnab Jana <arnab003jn.369@gmail.com>
2025-07-27 12:29:49 +0530

    2nd file

---
+++ 02_Data_Structures/Dictionary/02_Concatenation_Dictionaries.py
@@ -0,0 +1,19 @@
+dic1 = {
+   1: 10,
+   2: 20
+}
+
+dic2 = {
+   3: 30,
+   4: 40
+}
+
+dic_3 = {
+   5: 50,
+   6: 60
+}
+
+# Merging or concatenating dictionaries
+dic1.update(dic2)
+
+print(dic1)
