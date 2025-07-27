9d8c34c6d812b703e9d67ff96ad371318016fc2f
Arnab Jana <arnab003jn.369@gmail.com>
2025-07-27 20:14:02 +0530

    3rd file done

---
+++ 02_Data_Structures/String/03_First_2_Characters_n_Times.py
@@ -0,0 +1,9 @@
+String_1 = input("Enter a String: ")
+
+if len(String_1) < 2:
+   print("String must have at least 2 characters.")
+else:
+   first_two_chars = String_1[:2]
+   n = int(input("Enter the number of times to repeat the first two characters: "))
+   result = first_two_chars * n
+   print("Resulting String:", result)
