6135b85ed716899f1804c197935df9cf00c0e487
Arnab Jana <arnab003jn.369@gmail.com>
2025-07-27 19:39:20 +0530

    5th file done

---
+++ 02_Data_Structures/Tuple/05_List_of_Tuples_Values_Replace.py
@@ -0,0 +1,24 @@
+list_of_tuples = [(10, 20, 40), 
+                  (40, 50, 60), 
+                  (70, 80, 90)]
+
+print("The elements of List of Tuples are::\n", list_of_tuples, "\n")
+counter = 0
+for items in list_of_tuples:
+   counter += 1
+   print("Tuple ", counter, " = ", items)
+
+new_list = []
+# Replacing the last values of each tuple into 100
+for tuples in list_of_tuples:
+   # Convert tuple to list to modify
+   temp_list = list(tuples)
+   print(tuples)
+
+   temp_list[-1] = 100
+
+   # Convert back to tuple
+   temp_tuples = tuple(temp_list)
+   new_list.append(temp_tuples)
+
+print(list_of_tuples)
