06a588d022a9ca39e08eb2ff3f88c0633bcbff82
Arnab Jana <arnab003jn.369@gmail.com>
2025-07-27 10:26:55 +0530

    6th file chnages made

---
+++ 02_Data_Structures/List/06_New_Item_Second_Position.py
@@ -0,0 +1,12 @@
+list_new = input("Enter the elements of the list separated by commas: ").split(',')
+
+print("The elements in the list with respective positions before adding a new item are:")
+
+counter = 0
+for items in list_new:
+   counter += 1
+   print("Position = ",counter," ==> Element = ",items)
+   
+insert_item = input("Enter an elements to insert before the 2nd element in the exisitng list: ")  
+
+list_new.insert(1, insert_item)
