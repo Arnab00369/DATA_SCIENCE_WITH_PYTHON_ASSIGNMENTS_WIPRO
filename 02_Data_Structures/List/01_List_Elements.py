33d3286b2ba0a20ee7196c32f0aa15f88edec2eb
Arnab Jana <arnab003jn.369@gmail.com>
2025-07-27 22:26:19 +0530

    List 1 file

--- 02_Data_Structures/List/01_List_Elements.py
+++ 02_Data_Structures/List/01_List_Elements.py
@@ -1,5 +1,10 @@
+# Question 1:
+# Write a program to create a list of 5 integers and display the list items. Access individual elements through index.
+
+# Creating a list of 5 integers
 list_items = [111, 222, 333, 444, 555]
 
+# Displaying the list items
 print("The five elements in the list are as follows:")
 for items in list_items:
    print(items)
