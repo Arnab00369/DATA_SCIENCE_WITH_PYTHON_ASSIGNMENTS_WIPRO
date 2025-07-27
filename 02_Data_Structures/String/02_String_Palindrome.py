d0492a6e7e05b53cdd278a1cb6f5de8f1cc6bb7b
Arnab Jana <arnab003jn.369@gmail.com>
2025-07-27 20:07:30 +0530

    2nd file

--- 02_Data_Structures/String/02_String_Palindrome.py
+++ 02_Data_Structures/String/02_String_Palindrome.py
@@ -1,2 +1,7 @@
 String_1 = input("Enter a String to check if it is a Palindrome String or not: ")
 
+String_1 = String_1.lower()  # Convert to lowercase for case-insensitive comparison
+if String_1 == String_1[::-1]:
+      print(String_1," is a Palindrome String")
+else:
+      print(String_1," is not a Palindrome String")
