262c2ec29d1f881ecbcde7483a372b5769827151
Arnab Jana <arnab003jn.369@gmail.com>
2025-07-27 12:32:49 +0530

    modified

--- 02_Data_Structures/Dictionary/02_Concatenation_Dictionaries.py
+++ 02_Data_Structures/Dictionary/02_Concatenation_Dictionaries.py
@@ -2,18 +2,22 @@ dic1 = {
    1: 10,
    2: 20
 }
+print("Dictionary 1: ",dic1)
 
 dic2 = {
    3: 30,
    4: 40
 }
+print("Dictionary 2: ",dic2)
 
-dic_3 = {
+dic3 = {
    5: 50,
    6: 60
 }
+print("Dictionary 3: ",dic3)
 
 # Merging or concatenating dictionaries
+dic2.update(dic3)
 dic1.update(dic2)
 
-print(dic1)
+print("The key-value pairs after concatenating 3 dictionaries are: ",dic1)
