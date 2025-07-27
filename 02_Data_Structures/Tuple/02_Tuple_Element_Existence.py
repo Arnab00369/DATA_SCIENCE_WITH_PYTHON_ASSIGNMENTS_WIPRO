e8a289c65ebfb6db7e89daed70023607f95bcf9f
Arnab Jana <arnab003jn.369@gmail.com>
2025-07-27 19:11:45 +0530

    2nd file done

---
+++ 02_Data_Structures/Tuple/02_Tuple_Element_Existence.py
@@ -0,0 +1,6 @@
+tuple_1 = (10, 20, 30, 40, 50, 60, 70, 80, 90)
+print("The elements of Tuple are::\n", tuple_1, "\n")
+counter = 0
+for items in tuple_1:
+   counter += 1
+   print("Value ",counter," = ",items)
