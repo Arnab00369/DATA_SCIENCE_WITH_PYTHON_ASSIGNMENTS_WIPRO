6c2e6def34f41e7c5d52fcbd9e7d1a3d146123c6
Arnab Jana <arnab003jn.369@gmail.com>
2025-07-27 18:54:49 +0530

    done

---
+++ 02_Data_Structures/Dictionary/04_Dictionary_Keys_Values_Loops.py
@@ -0,0 +1,25 @@
+dict_new = {
+      "Name": "Arnab",
+      "Course": "Python",
+      3: "Data Structures",
+      4: 90,
+      5: 40.6
+}
+
+print("The elements of Dictionary are::\nKey Value Pair format::\n", dict_new,"\n")
+elements = 0
+for items in dict_new:
+   elements += 1
+   print("Position = ", elements, " ==> Key = ", items, "\twith Value = ", dict_new[items])
+
+counter = 0
+print("\nIterating and printing only keys:")
+for items in dict_new.keys():
+   counter += 1
+   print("Key ",counter," = ",items)
+
+counter_1 = 0
+print("\nIterating and printing only values:")
+for items in dict_new:
+   counter_1 += 1
+   print("Value ",counter_1," = ",dict_new[items])
