ff44d4d56a8de05cb7554ac057d661ec11619d4b
Arnab Jana <arnab003jn.369@gmail.com>
2025-07-29 19:07:27 +0530

    done

--- 03_Functions_Modules_and_Packages/01_Sum_Numbers.py
+++ 03_Functions_Modules_and_Packages/01_Sum_Numbers.py
@@ -1,12 +1,17 @@
 # Question 1:
+# Write a function to return the sum of all numbers in a list.
+# Sample List : (8, 2, 3, 0, 7)
+# Expected Output : 20
 
+# Defining the function to return the sum of all numbers in a list
 def sum_list(list_input):
    sum = 0
+   # Iterating through the list and adding each element to the sum
    for items in list_input:
       sum += int(items)
    return sum
 
+# Taking input from the user
 list_input = input("Enter the numbers in the list you want to add, separated by commas: ").split(',')
-
-
+# Displaying the required output
 print("The sum of the elements in the list are = ",sum_list(list_input))
--- 03_Functions_Modules_and_Packages/02_String_Reverse.py
+++ 03_Functions_Modules_and_Packages/02_String_Reverse.py
@@ -1,12 +1,16 @@
 # Question 2:
+# Write a function to return the reverse of a string.
+# Sample String : "1234abcd"
+# Expected Output : "dcba4321"
 
+# Defining the function to reverse a string
 def Str_Reverse(String_new):
    rev_str = ""
    rev_str = String_new[::-1]
    return rev_str
 
+
+# Taking input from the user
 String_new = input("Enter a String: ")
-
+# Calling the function to reverse the string
 print("The reverse of the string is: ", Str_Reverse(String_new))
-
-
