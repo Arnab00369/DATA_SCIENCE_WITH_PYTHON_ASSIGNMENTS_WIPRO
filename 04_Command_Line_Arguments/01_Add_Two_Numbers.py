148b9e161a978f6856834619dbe6490258a99c95
Arnab Jana <arnab003jn.369@gmail.com>
2025-08-01 18:02:32 +0530

    Cmd lines codes

---
+++ 04_Command_Line_Arguments/01_Add_Two_Numbers.py
@@ -0,0 +1,4 @@
+from sys import argv
+print("\nOUTPUT::\nNumber 1 = ", argv[1])
+print("Number 2 = ", argv[2])
+print("Sum of given two numbers = ", int(argv[1]) + int(argv[2]))
---
+++ 04_Command_Line_Arguments/02_Welcome_Message.py
@@ -0,0 +1,3 @@
+from sys import argv
+print("\nOUTPUT::\nWelcome to the Command Line Arguments Program!!")
+print("The File Name is = ",argv[0])
